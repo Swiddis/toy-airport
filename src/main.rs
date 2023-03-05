a1e0789e5a45fd7b67b92d280e310a12306ee8f1
Simeon Widdis <sawiddis@gmail.com>
2023-03-05 09:28:24 -0800

    Smooth drawn plane path via hermite spline

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use crate::atc::planning::compute_landing_plan;
 use crate::simulate::airport::Airport;
 use crate::simulate::plane::Plane;
 use lyon_geom::{Point, Vector};
+use nannou::geom::{path::Builder, Path};
 use nannou::prelude::*;
 
+const SCALE: f32 = 10.0;
+
 struct Model {
-    path: Vec<Point<i32>>
+    steps: Vec<Plane>,
 }
 
 fn main() {
@@ -17,23 +20,42 @@ fn main() {
 
 fn model(_app: &App) -> Model {
     Model {
-        path: generate_plane_path()
+        steps: generate_plane_steps(),
     }
 }
 
-fn event(_app: &App, _model: & mut Model, _event: Event) {}
+impl Model {
+    fn as_smooth_path(&self) -> Path {
+        let builder = Builder::new();
+        let start = self.steps[0].position.to_f32() * SCALE;
+        let mut builder = builder.begin(pt2(start.x, start.y));
+        for (p1, p2) in self.steps.iter().zip(self.steps.iter().skip(1)) {
+            let (c1, c2, to) = (
+                p1.position.to_f32() * SCALE + p1.velocity.to_f32() * SCALE / 3.0,
+                p2.position.to_f32() * SCALE - p2.velocity.to_f32() * SCALE / 3.0,
+                p2.position.to_f32() * SCALE,
+            );
+            let (c1, c2, to) = (pt2(c1.x, c1.y), pt2(c2.x, c2.y), pt2(to.x, to.y));
+            builder = builder.cubic_bezier_to(c1, c2, to);
+        }
+        builder.build()
+    }
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    for (a, b) in model.path.iter().zip(model.path.iter().skip(1)) {
-        let (a, b) = ((*a * 10).to_f32(), (*b * 10).to_f32());
-        let (pt_a, pt_b) = (pt2(a.x, a.y), pt2(b.x, b.y));
-        draw.line().start(pt_a).end(pt_b).weight(4.0).color(STEELBLUE);
-    }
+    let path = model.as_smooth_path();
+    draw.path()
+        .stroke()
+        .weight(4.0)
+        .color(STEELBLUE)
+        .events(path.iter());
     draw.to_frame(app, &frame).unwrap();
 }
 
-fn generate_plane_path() -> Vec<Point<i32>> {
+fn generate_plane_steps() -> Vec<Plane> {
     let airport = Airport {
         position: Point::new(10, 0),
         runway_direction: Vector::new(1, -1),
@@ -43,5 +65,5 @@ fn generate_plane_path() -> Vec<Point<i32>> {
         velocity: Vector::new(10, 0),
     };
     let plan = compute_landing_plan(&plane, &airport).unwrap();
-    plan.0.into_iter().map(|p| p.position).collect()
+    plan.0
 }
