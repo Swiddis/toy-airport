0320524f0fe4c76551f811e360c0dd89d9377d54
Simeon Widdis <sawiddis@gmail.com>
2023-03-05 08:36:43 -0800

    Add graphics with Nannou

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,32 @@ use crate::atc::planning::compute_landing_plan;
 use crate::simulate::airport::Airport;
 use crate::simulate::plane::Plane;
 use lyon_geom::{Point, Vector};
+use nannou::prelude::*;
+
+struct Model {
+    path: Vec<Point<i32>>
+}
 
 fn main() {
-    let path = generate_plane_path();
-    println!("Landed in {} steps", path.len());
-    println!(
-        "Steps: {:?}",
-        path.iter().map(|p| (p.x, p.y)).collect::<Vec<(i32, i32)>>()
-    )
+    nannou::app(model).event(event).simple_window(view).run();
+}
+
+fn model(_app: &App) -> Model {
+    Model {
+        path: generate_plane_path()
+    }
+}
+
+fn event(_app: &App, _model: & mut Model, _event: Event) {}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+    for (a, b) in model.path.iter().zip(model.path.iter().skip(1)) {
+        let (a, b) = ((*a * 10).to_f32(), (*b * 10).to_f32());
+        let (pt_a, pt_b) = (pt2(a.x, a.y), pt2(b.x, b.y));
+        draw.line().start(pt_a).end(pt_b).weight(4.0).color(STEELBLUE);
+    }
+    draw.to_frame(app, &frame).unwrap();
 }
 
 fn generate_plane_path() -> Vec<Point<i32>> {
